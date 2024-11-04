12d7203c62f47074bf8544bb0ba35f5ed196eabd
Alok kannaujia <alokabc25@gmail.com>
2024-11-04 21:38:09 +0530

    update

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -45,12 +45,12 @@ const Page = async ({ params, searchParams }: any) => {
           <div className="flex justify-end">
             <Votes
               type="Question"
-              itemId={JSON.stringify(result._id)}
-              userId={JSON.stringify(mongoUser._id)}
-              upvotes={result.upvotes.length}
-              hasupVoted={result.upvotes.includes(mongoUser._id)}
-              downvotes={result.downvotes.length}
-              hasdownVoted={result.downvotes.includes(mongoUser._id)}
+              itemId={JSON.stringify(result?._id)}
+              userId={JSON.stringify(mongoUser?._id)}
+              upvotes={result?.upvotes.length}
+              hasupVoted={result?.upvotes.includes(mongoUser?._id)}
+              downvotes={result?.downvotes.length}
+              hasdownVoted={result?.downvotes.includes(mongoUser?._id)}
               hasSaved={mongoUser?.saved.includes(result._id)}
             />
           </div>
@@ -89,26 +89,26 @@ const Page = async ({ params, searchParams }: any) => {
       <div className="mt-8 flex flex-wrap gap-2">
         {result.tags.map((tag: any) => (
           <RenderTag
-            key={tag._id}
-            _id={tag._id}
-            name={tag.name}
+            key={tag?._id}
+            _id={tag?._id}
+            name={tag?.name}
             showCount={false}
           />
         ))}
       </div>
 
       <AllAnswers
-        questionId={result._id}
-        userId={mongoUser._id}
-        totalAnswers={result.answers.length}
+        questionId={result?._id}
+        userId={mongoUser?._id}
+        totalAnswers={result?.answers.length}
         page={searchParams?.page}
         filter={searchParams?.filter}
       />
 
       <Answer
-        question={result.content}
-        questionId={JSON.stringify(result._id)}
-        authorId={JSON.stringify(mongoUser._id)}
+        question={result?.content}
+        questionId={JSON.stringify(result?._id)}
+        authorId={JSON.stringify(mongoUser?._id)}
       />
     </>
   );
